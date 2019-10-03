98a12b7ded5390b2ded92ffda3ad0c9a239c5660
EvanStJohn <46731712+EvanStJohn@users.noreply.github.com>
2019-10-03 16:06:03 -0700

    finished lab
    complete

--- L03.cpp
+++ L03.cpp
@@ -3,12 +3,15 @@
 using namespace std;
 
 class Trip {
+private:
+    int first = 0, second = 0, third = 0;
+    int CHARGE = 100;
 public:
     void readFile();
-    int chargeFee();
-    int chargeFee(bool discount);
+    void chargeFee();
+    void chargeFee(bool discount);
     int revenue();
-    void output(int cash);
+    void output();
 };
 
 void Trip::readFile()
@@ -16,33 +19,54 @@ void Trip::readFile()
     ifstream in_stream;
     in_stream.open("input.txt");
 
-    int first = 0, second = 0, third = 0;
     in_stream >> first >> second >> third;
 
     in_stream.close();
 }
 
-int Trip::chargeFee() // no discount
+void Trip::chargeFee() // no discount
 {
-
-
-    return 100;
+    second = second * CHARGE;
 }
 
-int Trip::chargeFee(bool discount) // true = discount, false = fee
+void Trip::chargeFee(bool discount) // true = discount, false = fee
 {
     if (discount)
     {
-        return
+        first = first * (CHARGE - 15);
     }
     else
     {
-
+        third = third * (CHARGE + 10);
     }
+}
+
+int Trip::revenue()
+{
+    return first + second + third;
+}
+
+void Trip::output()
+{
+    ofstream out_stream;
+    out_stream.open("output.txt");
+
+    out_stream << "Day 1 revenue: " << first << "\n"
+            << "Day 2 revenue: " << second << "\n"
+            << "Day 3 revenue: " << third << "\n"
+            << "Total Revenue: " << revenue()
+            << endl;
+
+    out_stream.close();
 }
 
 int main() {
     Trip trip;
     trip.readFile();
+    trip.chargeFee();
+    trip.chargeFee(true);
+    trip.chargeFee(false);
+    trip.output();
+
     return 0;
 }
